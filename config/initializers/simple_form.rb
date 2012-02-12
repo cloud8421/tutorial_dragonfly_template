162bb84c82f44b186d1ab09b6677f5e97ce78af4
Claudio Ortolina <claudio@jumpzero.com>
2012-02-12 10:40:10 +0000

    Update simple form initializer for Ruby 1.9

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -5,8 +5,8 @@ SimpleForm.setup do |config|
   # wrapper, change the order or even add your own to the
   # stack. The options given below are used to wrap the
   # whole input.
-  config.wrappers :default, :class => :input,
-    :hint_class => :field_with_hint, :error_class => :field_with_errors do |b|
+  config.wrappers :default, class: :input,
+    hint_class: :field_with_hint, error_class: :field_with_errors do |b|
     ## Extensions enabled by default
     # Any of these extensions can be disabled for a
     # given input by passing: `f.input EXTENSION_NAME => false`.
@@ -18,7 +18,7 @@ SimpleForm.setup do |config|
     b.use :html5
 
     # Calculates placeholders automatically from I18n
-    # You can also pass a string as f.input :placeholder => "Placeholder"
+    # You can also pass a string as f.input placeholder: "Placeholder"
     b.use :placeholder
 
     ## Optional extensions
@@ -41,41 +41,41 @@ SimpleForm.setup do |config|
 
     ## Inputs
     b.use :label_input
-    b.use :hint,  :tag => :span, :class => :hint
-    b.use :error, :tag => :span, :class => :error
+    b.use :hint,  tag: :span, class: :hint
+    b.use :error, tag: :span, class: :error
   end
 
-  config.wrappers :bootstrap, :tag => 'div', :class => 'control-group', :error_class => 'error' do |b|
+  config.wrappers :bootstrap, tag: 'div', class: 'control-group', error_class: 'error' do |b|
     b.use :placeholder
-    b.use :label, :class => 'control-label'
-    b.use :tag => 'div', :class => 'controls' do |ba|
+    b.use :label, class: 'control-label'
+    b.use tag: 'div', class: 'controls' do |ba|
       ba.use :input
-      ba.use :error, :tag => 'span', :class => 'help-inline'
-      ba.use :hint,  :tag => 'p', :class => 'help-block'
+      ba.use :error, tag: 'span', class: 'help-inline'
+      ba.use :hint,  tag: 'p', class: 'help-block'
     end
   end
 
-  config.wrappers :prepend, :tag => 'div', :class => "control-group", :error_class => 'error' do |b|
+  config.wrappers :prepend, tag: 'div', class: "control-group", error_class: 'error' do |b|
     b.use :placeholder
-    b.use :label, :class => 'control-label'
-    b.use :tag => 'div', :class => 'controls' do |input|
-      input.use :tag => 'div', :class => 'input-prepend' do |prepend|
+    b.use :label, class: 'control-label'
+    b.use tag: 'div', class: 'controls' do |input|
+      input.use tag: 'div', class: 'input-prepend' do |prepend|
         prepend.use :input
       end
-      input.use :hint,  :tag => 'span', :class => 'help-block'
-      input.use :error, :tag => 'span', :class => 'help-inline'
+      input.use :hint,  tag: 'span', class: 'help-block'
+      input.use :error, tag: 'span', class: 'help-inline'
     end
   end
 
-  config.wrappers :append, :tag => 'div', :class => "control-group", :error_class => 'error' do |b|
+  config.wrappers :append, tag: 'div', class: "control-group", error_class: 'error' do |b|
     b.use :placeholder
-    b.use :label, :class => 'control-label'
-    b.use :tag => 'div', :class => 'controls' do |input|
-      input.use :tag => 'div', :class => 'input-append' do |append|
+    b.use :label, class: 'control-label'
+    b.use tag: 'div', class: 'controls' do |input|
+      input.use tag: 'div', class: 'input-append' do |append|
         append.use :input
       end
-      input.use :hint,  :tag => 'span', :class => 'help-block'
-      input.use :error, :tag => 'span', :class => 'help-inline'
+      input.use :hint,  tag: 'span', class: 'help-block'
+      input.use :error, tag: 'span', class: 'help-inline'
     end
   end
 
@@ -87,8 +87,8 @@ SimpleForm.setup do |config|
 
   # Define the way to render check boxes / radio buttons with labels.
   # Defaults to :nested for bootstrap config.
-  #   :inline => input + label
-  #   :nested => label > input
+  #   inline: input + label
+  #   nested: label > input
   config.boolean_style = :nested
 
   # Default class for buttons
