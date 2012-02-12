9f6bf26013c5819692462b6d04b401069e1f0b8a
Claudio Ortolina <claudio@jumpzero.com>
2012-02-12 12:05:35 +0000

    Fixed user spec.
    Signed-off-by: Claudio Ortolina <claudio@jumpzero.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe User do
 
+  let(:user) { Factory(:user, first_name: "John", last_name: "Doe") }
+
 	context "validations" do
 		
     let!(:user) {Factory(:user)}
@@ -11,20 +13,19 @@ describe User do
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
 
-	end
+  end
 
-	context "attributes" do
+  context "attributes" do
 
-		let(:user) { Factory(:user, first_name: "John", last_name: "Doe") }
+    it "should have a name attribute" do
+      user.should respond_to(:name)
+    end
 
-		it "should have a name attribute" do
-			user.should respond_to(:name)
-		end
+    it "should have the right name" do
+      user.name.should eq('John Doe')
+    end
 
-		it "should have the right name" do
-			user.name.should eq('John Doe')
-		end
+ end
 
-	end  
 
 end
